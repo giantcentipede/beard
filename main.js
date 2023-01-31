1df71e6a5bf0018d4c7ae7236379ea34c7c97110
giantcentipede <97794708+giantcentipede@users.noreply.github.com>
2023-01-31 16:56:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+
+}
+
+function setup() {
+canvas=createCanvas(300,300);
+canvas.center();
+
+}
+
+function draw() {
+    
+}
+
+function press() {
+save("your_beard.png");
+}
